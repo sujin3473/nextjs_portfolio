e73a4ccede2c5d222f01d297ad88979bf9b2b055
ysj <sujin3473@gmail.com>
2022-07-09 15:53:14 +0900

    레이아웃 설정

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,59 @@
 export default function Footer(): React.ReactElement {
-  return <>footer</>;
+  return (
+    <>
+      <footer className="text-gray-600 body-font">
+        <div className="bg-gray-100">
+          <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
+            <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+              <span className="ml-3 text-xl">Sienna's Portfolio</span>
+            </a>
+            <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
+              © 2022 Tailblocks —
+              <a
+                href="https://twitter.com/knyttneve"
+                rel="noopener noreferrer"
+                className="text-gray-600 ml-1"
+                target="_blank"
+              >
+                @knyttneve
+              </a>
+            </p>
+            <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+              <a className="ml-3 text-gray-500">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                  <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                </svg>
+              </a>
+              <a className="ml-3 text-gray-500">
+                <svg
+                  fill="currentColor"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="0"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="none"
+                    d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                  ></path>
+                  <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                </svg>
+              </a>
+            </span>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
--- components/layout.tsx
+++ components/layout.tsx
@@ -10,7 +10,6 @@ export default function Layout({
   return (
     <>
       <Header />
-      <h1>레이아웃</h1>
       <div>{children}</div>
       <Footer />
     </>
